822179016d2d3e64e73e6c1a6272e783dab35ee5
Raul <you@example.com>
2024-02-22 18:10:32 +0100

    add conditional rendering based on marker reconeissance

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,29 @@ function App() {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div id="container">
-      <div id="canvas">
+    <div id="container" style={{ width: "100vw", height: "100vh" }}>
+      <div id="canvas" style={{ width: "50vw", height: "50vh" }}>
         <ARCanvas
+          width="100px"
+          height="100px"
           camera={{ position: [0, 0, 0] }}
-          onCreated={({ gl }: { gl: THREE.WebGLRenderer }) => gl.setSize(100, 100)}>
+          onCreated={({ gl }: { gl: THREE.WebGLRenderer }) => gl.setSize(50, 50)}>
           <ambientLight />
           <pointLight position={[10, 10, 0]} />
           <ARMarker
             type={'pattern'}
             patternUrl={"data/hiro.patt"}
             onMarkerFound={() => {
-              // alert("Marker Found")
+              console.log(visible);
+
+              console.log("Marker Found")
               setVisible(true);
+            }}
+            onMarkerLost={() => {
+              console.log(visible);
+
+              console.log("Marker Lost")
+              setVisible(false);
             }}
           >
             <mesh>
@@ -35,7 +45,7 @@ function App() {
 
         </ARCanvas>
       </div>
-      {visible && <div id="text">
+      {visible && <div id="text" style={{ fontWeight: "bold" }}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis similique, eos magni tempore tempora odio enim ipsam ipsum cupiditate temporibus dolorum error magnam fugiat id modi. Tenetur animi similique nisi.
       </div>}
     </div>
